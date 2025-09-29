72af9c377d67ddbdf11cc35684a606f308a24cc7
Sam <bamiseoluwayinka069@gmail.com>
2025-09-29 20:04:47 +0100

    Progress made

--- PROGRESS.md
+++ PROGRESS.md
@@ -3,7 +3,8 @@
 <body>
 
 <ul>
-  <li></li>
+  <li>11:05. I need to draw lines as the ball moves in space</li>
+  </li>20:03 Solved the quadratic path projectile issue but there is still some features to be added onto it</li>
 </ul>
 
 # Copyright (c) 2024 Sam
--- Projectiles.py
+++ Projectiles.py
@@ -2,20 +2,20 @@ import pygame, math
 
 pygame.init()
 
-screen = pygame.display.set_mode((900,400))
+screen = pygame.display.set_mode((1400,800))
 pygame.display.set_caption("Projectile")
 
 running = True
 
-X = 0+8
-Y = 300-8
+#0.01px per frame <==> 1m/s
 
-Degree = 4
+Degree = -15
 Radians = math.radians(Degree)
-InitialVelocity = 10 #0.1px per frame <==> 10m/s
-HorizontalVelocity = X + InitialVelocity * math.cos(Radians)
-VerticalVelocity = Y + InitialVelocity * math.sin(Radians)
-Acceleration = 9.81
+InitialVelocity = 12 # 1000m/s
+HorizontalPosition = 0
+VerticalPosition = 0
+Acceleration = 0.981 # 9.81m/s^2
+time = 0
 
 while running:
 	for event in pygame.event.get():
@@ -23,10 +23,17 @@ while running:
 			running = False
 
 	screen.fill((0,0,0))
-	pygame.draw.circle(screen, (255,255,255), (HorizontalVelocity,VerticalVelocity), 8, 8)
-	HorizontalVelocity+=0.01 * InitialVelocity * math.cos(Radians)
-	# VerticalVelocity-=0.01 * InitialVelocity * math.sin(Radians)
-	# pygame.draw.line(screen, (255,255,255), (HorizontalVelocity, VerticalVelocity), (HorizontalVelocity+0.01, VerticalVelocity))
+	pygame.draw.circle(screen, (255,255,255), (HorizontalPosition,VerticalPosition), 8, 8)
+	HorizontalPosition = time * (InitialVelocity * math.cos(Radians)) + 300 # Position (Distance) = Incrementation (px movement per frame) (Time) * Velocity
+	VerticalPosition = ((time * InitialVelocity * math.sin(Radians)) + 0.5 * (Acceleration) * (time**2)) + 100 # S = Ut + (1/2)(g)(t^2) + C
+	time+=0.02
+
+	print(f"Rate of change of vertical displacement or velocity: {(((time+0.02) * InitialVelocity * math.sin(Radians)) + 0.5 * (Acceleration) * ((time+0.02)**2) + 100)- ((time * InitialVelocity * math.sin(Radians) + 0.5 * (Acceleration) * (time**2) + 100))} at time = {time}")
+	V = (((time+0.02) * InitialVelocity * math.sin(Radians)) + 0.5 * (Acceleration) * ((time+0.02)**2) + 100)- ((time * InitialVelocity * math.sin(Radians) + 0.5 * (Acceleration) * (time**2) + 100))
+	V2 = (((time+0.04) * InitialVelocity * math.sin(Radians)) + 0.5 * (Acceleration) * ((time+0.04)**2) + 100) - (((time+0.02) * InitialVelocity * math.sin(Radians)) + 0.5 * (Acceleration) * ((time+0.02)**2) + 100)
+	
+	print(f"Rate of change of rate of change of displacement or Acceleration: {V2 - V}")
+	# Drawing angle line
 	# screen.draw.fill_circle((0+8,300-8), 8, (255,0,0))
 	# pygame.draw.rect(screen, (0,0,0), (0, 300, 900, 100))
 	pygame.display.update()
