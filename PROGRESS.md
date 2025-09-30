aaaa1bb8255b5970002fa427a178d639239b6c71
Sam <bamiseoluwayinka069@gmail.com>
2025-09-30 20:54:18 +0100

    Made Progress

--- PROGRESS.md
+++ PROGRESS.md
@@ -5,6 +5,7 @@
 <ul>
   <li>11:05. I need to draw lines as the ball moves in space</li>
   </li>20:03 Solved the quadratic path projectile issue but there is still some features to be added onto it</li>
+<li>20:46 Getting closer to making the simulation more real world based 30/09/25</li>
 </ul>
 
 # Copyright (c) 2024 Sam
--- Projectiles.py
+++ Projectiles.py
@@ -1,21 +1,24 @@
-import pygame, math
+import pygame, math, time as Time
 
 pygame.init()
 
-screen = pygame.display.set_mode((1400,800))
+dimensions = pygame.display.get_desktop_sizes()
+screen = pygame.display.set_mode((dimensions[0][0], dimensions[0][1]-60))
 pygame.display.set_caption("Projectile")
 
 running = True
 
-#0.01px per frame <==> 1m/s
-
-Degree = -15
+Degree = -45
 Radians = math.radians(Degree)
-InitialVelocity = 12 # 1000m/s
+InitialVelocity = 30 # 30 m/s
 HorizontalPosition = 0
 VerticalPosition = 0
-Acceleration = 0.981 # 9.81m/s^2
+Acceleration = 9.81 # 9.81m/s^2
+Acceleration*=0.1
+ground = 400
 time = 0
+
+Start = Time.time()
 
 while running:
 	for event in pygame.event.get():
@@ -23,17 +26,32 @@ while running:
 			running = False
 
 	screen.fill((0,0,0))
-	pygame.draw.circle(screen, (255,255,255), (HorizontalPosition,VerticalPosition), 8, 8)
-	HorizontalPosition = time * (InitialVelocity * math.cos(Radians)) + 300 # Position (Distance) = Incrementation (px movement per frame) (Time) * Velocity
-	VerticalPosition = ((time * InitialVelocity * math.sin(Radians)) + 0.5 * (Acceleration) * (time**2)) + 100 # S = Ut + (1/2)(g)(t^2) + C
-	time+=0.02
 
-	print(f"Rate of change of vertical displacement or velocity: {(((time+0.02) * InitialVelocity * math.sin(Radians)) + 0.5 * (Acceleration) * ((time+0.02)**2) + 100)- ((time * InitialVelocity * math.sin(Radians) + 0.5 * (Acceleration) * (time**2) + 100))} at time = {time}")
-	V = (((time+0.02) * InitialVelocity * math.sin(Radians)) + 0.5 * (Acceleration) * ((time+0.02)**2) + 100)- ((time * InitialVelocity * math.sin(Radians) + 0.5 * (Acceleration) * (time**2) + 100))
-	V2 = (((time+0.04) * InitialVelocity * math.sin(Radians)) + 0.5 * (Acceleration) * ((time+0.04)**2) + 100) - (((time+0.02) * InitialVelocity * math.sin(Radians)) + 0.5 * (Acceleration) * ((time+0.02)**2) + 100)
-	
-	print(f"Rate of change of rate of change of displacement or Acceleration: {V2 - V}")
-	# Drawing angle line
-	# screen.draw.fill_circle((0+8,300-8), 8, (255,0,0))
-	# pygame.draw.rect(screen, (0,0,0), (0, 300, 900, 100))
+
+	if VerticalPosition <= ground: 
+		pygame.draw.circle(screen, (255,255,255), (HorizontalPosition,VerticalPosition), 8, 8)
+		HorizontalPosition = time * (InitialVelocity * math.cos(Radians)) + 300 # Position (Distance) = Incrementation (px movement per frame) (Time) * Velocity
+		VerticalPosition = ((time * InitialVelocity * math.sin(Radians)) + 0.5 * (Acceleration) * (time**2)) + ground # S = Ut + (1/2)(g)(t^2) + C
+		time+=0.0356 # = 1.9047, y = 4.32
+
+		# Testing
+		print(f"Rate of change of vertical displacement or velocity: {(((time+0.02) * InitialVelocity * math.sin(Radians)) + 0.5 * (Acceleration) * ((time+0.02)**2) + 100)- ((time * InitialVelocity * math.sin(Radians) + 0.5 * (Acceleration) * (time**2) + 100))} at time = {time}")
+		V = (((time+0.02) * InitialVelocity * math.sin(Radians)) + 0.5 * (Acceleration) * ((time+0.02)**2) + 100)- ((time * InitialVelocity * math.sin(Radians) + 0.5 * (Acceleration) * (time**2) + 100))
+		V2 = (((time+0.04) * InitialVelocity * math.sin(Radians)) + 0.5 * (Acceleration) * ((time+0.04)**2) + 100) - (((time+0.02) * InitialVelocity * math.sin(Radians)) + 0.5 * (Acceleration) * ((time+0.02)**2) + 100)
+		
+		print(f"Rate of change of rate of change of displacement or Acceleration: {V2 - V}")
+	else: 
+		End = Time.time()
+		print(End-Start)
+
+		print(f"The times when it was on the ground are {0.00} and {time}")
+		print(f"Time taken to reach Max Height is {time/2}")
+		print(f'Horizontal distance is {HorizontalPosition}')
+		pygame.draw.circle(screen, (255,255,255), (HorizontalPosition,VerticalPosition), 8, 8)
+
+		running = False #Testing
+
+	# Drawing the ground
+	pygame.draw.line(screen, (255,255,255), (0,ground+8), (dimensions[0][0], ground+8))
+
 	pygame.display.update()
